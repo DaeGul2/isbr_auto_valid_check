137369b26226a5fb7ebafaa27630a5b63dd0f76b
민태희 <122341071+DaeGul2@users.noreply.github.com>
2025-05-28 17:00:11 +0900

    ㅇ
    ㅇ

--- functions/utils.js
+++
@@ -1,9 +0,0 @@
-// utils.js
-const path = require('path');
-
-// 공통 경로 생성 함수
-function getResultScreenshotPath(screenshotDir, item) {
-  return path.join(screenshotDir, `${item.registerationNumber}_${item.name}_${item.certificateName}.png`);
-}
-
-module.exports = { getResultScreenshotPath };
